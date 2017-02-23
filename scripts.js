019cf002087755969a4715afca4dd80bb34cefc0
Morris Fuller <morrisjfuller@gmail.com>
2017-02-23 10:43:04 -0800

    2nd commit, basic basic functionality

--- scripts.js
+++ scripts.js
@@ -2,15 +2,17 @@
  * Created by h205p3 on 2/7/17.
  */
 $(document).ready(function() {
-    $("button").click(function() {
+    $("#search").click(function() {
+        $("#output").empty();
+        $("#display").empty();
         $.ajax({
-            url: "https://launchlibrary.net/1.1/launch/next/1",
+            url: "https://launchlibrary.net/1.1/launch/next/"+document.getElementById("numlaunches").value,
             type: 'GET',
             crossDomain: true,
             dataType: 'json',
             success: function (result) {
-                var launch = result.launches[1];
-                document.getElementById("output").innerHTML = launch
+
+                output(result)
             },
             error: function () {
                 document.getElementById("output").innerHTML = "Failed!"
@@ -18,4 +20,46 @@ $(document).ready(function() {
         })
     })
 });
+
+function output(result){
+    for (var i = 0; i < document.getElementById("numlaunches").value; i++) {
+        var button = document.createElement("button");
+        button.innerHTML = result.launches[i].name;
+        button.id = "button"+i;
+        document.getElementById("output").appendChild(button);
+        //button.onclick = function(){display(button.id, result)}
+    }
+}
+
+//separate onclick iteration, overwritten by each iteration^^?
+
+function display(id, result){
+    var realID = id.substring(6, id.length);
+
+    $("#display").empty();
+    var highResult = result.launches[realID].rocket.name;
+    var nameArr = [];
+    var lowResult = highResult.toLowerCase();
+
+
+    for (var i = 0; i < lowResult.length; i++){
+         nameArr.push(lowResult[i])
+    }
+
+    if (nameArr[0] === ("a" || "e" || "i" || "o" || "u" || "y")) {
+        document.getElementById("display").innerHTML = result.launches[realID].missions[0].name + " is launching on an " + result.launches[realID].rocket.name + " rocket. It will launch between " + result.launches[realID].windowstart + " and " + result.launches[realID].windowend + ". " + result.launches[realID].missions[0].description
+
+    }
+    else{
+        document.getElementById("display").innerHTML = result.launches[realID].missions[0].name + " is launching on a " + result.launches[realID].rocket.name + " rocket. It will launch between " + result.launches[realID].windowstart + " and " + result.launches[realID].windowend + ". " + result.launches[realID].missions[0].description
+    }
+}
+
+
+
+
+
+
+
+
 
